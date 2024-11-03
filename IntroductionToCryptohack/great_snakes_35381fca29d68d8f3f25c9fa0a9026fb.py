d4c7f82401f7f85dfbbe506491525d0824a968dd
raflyrahila <69660299+wreeckitralph@users.noreply.github.com>
2024-11-03 15:08:08 +0700

    introduction to cryptohack

--- IntroductionToCryptohack/great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
+++ IntroductionToCryptohack/great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-
 import sys
-# import this
+
 
 if sys.version_info.major == 2:
     print("You are running Python 2, which is no longer supported. Please update to Python 3.")
@@ -10,3 +8,5 @@ ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 
 
 print("Here is your flag:")
 print("".join(chr(o ^ 0x32) for o in ords))
+
+# flag : crypto{z3n_0f_pyth0n}
